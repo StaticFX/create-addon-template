0f0c7d95406d43ec6290018415b90e1f28e9aea8
devin <devinfritz@gmx.de>
2026-07-11 15:58:32 +0000

    Add shaft model to renderer

--- src/main/java/com/example/examplemod/content/kinetics/ExampleShaftRenderer.java
+++ src/main/java/com/example/examplemod/content/kinetics/ExampleShaftRenderer.java
@@ -1,8 +1,11 @@
 package com.example.examplemod.content.kinetics;
 
+import com.simibubi.create.AllPartialModels;
 import com.simibubi.create.content.kinetics.base.KineticBlockEntity;
 import com.simibubi.create.content.kinetics.base.KineticBlockEntityRenderer;
 
+import net.createmod.catnip.render.CachedBuffers;
+import net.createmod.catnip.render.SuperByteBuffer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.world.level.block.state.BlockState;
 
@@ -23,5 +26,10 @@ public class ExampleShaftRenderer<T extends KineticBlockEntity> extends KineticB
     @Override
     protected BlockState getRenderedBlockState(T be) {
         return shaft(getRotationAxisOf(be));
+    }
+
+    @Override
+    protected SuperByteBuffer getRotatedModel(T be, BlockState state) {
+        return CachedBuffers.partial(AllPartialModels.SHAFT, state);
     }
 }
